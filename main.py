3b90196b27dfdfc5755e6f662e4ebfec7d5c72f9
Jesse Kleemann <j.kleemann@tu-bs.de>
2021-09-01 23:49:17 +0200

    sh10rt

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from pathlib import Path
+
 from SungrowModbusTcpClient import SungrowModbusTcpClient
 from mapping import lengthMapping, sungrowIp, unitMapping, factorMapping, addressMapping
+import csv
 
 
 def getInputRegisterValue(address, length, factor):
@@ -38,12 +41,41 @@ def printHoldingValue(valueName):
     print(valueName + ": " + value + " " + unit)
 
 
+def txt_to_solariot():
+    with open(Path('documentation') / 'eingangsregister_stand2021_02_02.txt') as csvfile:
+        reader = csv.reader(csvfile, delimiter='\t')
+        header = []
+        output = {}
+        for i, row in enumerate(reader):
+            if i == 0:
+                header = row
+                print(header)
+            if i > 0:
+                factor = row[header.index('factor')]
+                if len(factor) == 0:
+                    factor = 1
+                print(factor)
+                factor = float(factor)
+                factor = 1 / factor
+                if factor == 1:
+                    factor = ''
+                else:
+                    factor = f'_{factor}'
+                name = str(row[header.index('name')]).replace('  ', ' ').replace('  ', ' ').replace(' ', '_').lower()
+                address = int(row[header.index('address')]) + 1
+                output[str(address)] = f'{name}{factor}'
+                # print(row[header.index('deviceId')])
+        print(output)
+
+
 if __name__ == '__main__':
-    client = SungrowModbusTcpClient(sungrowIp)
-    running = True
+    txt_to_solariot()
+
+    # client = SungrowModbusTcpClient(sungrowIp)
+    # running = True
 
     # print(getHoldingValue("Voltage Phase A"))
     # print(getHoldingValue("Voltage Phase B"))
     # print(getHoldingValue("Voltage Phase C"))
 
-    client.close()
+    # client.close()
